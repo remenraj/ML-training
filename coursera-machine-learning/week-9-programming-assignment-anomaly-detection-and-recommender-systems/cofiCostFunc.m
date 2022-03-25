1ea62a228d46c9d3febb8103d4c98f68269609f7
remenraj <remenraj@gmail.com>
2022-03-25 14:32:13 +0530

    Add week 9 assignment on recommender systems

--- coursera-machine-learning/week-9-programming-assignment-anomaly-detection-and-recommender-systems/cofiCostFunc.m
+++ coursera-machine-learning/week-9-programming-assignment-anomaly-detection-and-recommender-systems/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -26,11 +26,12 @@ Theta_grad = zeros(size(Theta));
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
 %
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
+% Notes: X - num_movies  x num_features matrix of movie features, each row is a movie
+%        Theta - num_users  x num_features matrix of user features, each row is an user
 %        Y - num_movies x num_users matrix of user ratings of movies
+%            where Y(i,j)= (movie x user) = movie rating by the user
 %        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
+%            i-th movie was rated by the j-th user, else R(i, j) = 0
 %
 % You should set the following variables correctly:
 %
@@ -40,19 +41,39 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% Error is a matrix of order [num_movies x num_users]
+Error = (X * Theta') - Y;
+
+%%%%%%%%%%%%%%%%%%%%%%$$$ Without Regularization %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Collaborative filtering cost function
+% J is a scalar value
+J = (1/2) * sum(sum( (Error).^2 .* R ));
 
 
+%%% gradient without regularization %%%
+% X_grad is a matrix of same order of X which is [num_movies x num_features]
+X_grad = (Error .* R) * Theta;
 
+% Theta_grad is a matrix of same oroder of Theta which is [num_users x num_features]
+Theta_grad = (Error .* R)' * X;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%% With Regularization %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Regularization term for X
+reg_term_theta = (lambda/2) * sum(sum(Theta .^ 2));
 
+% Regularization term for X
+reg_term_x = (lambda/2) * sum(sum(X .^ 2));
 
+% Collaborative filtering cost function with regularization
+J = J + reg_term_theta + reg_term_x;
 
-
-
-
-
+%%% gradient with regularization %%%
+X_grad = X_grad + (lambda * X);
+Theta_grad = Theta_grad + (lambda * Theta);
 
 
 % =============================================================
